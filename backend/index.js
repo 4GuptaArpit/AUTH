9783b094e8cd9b5944f929151072f6a6c97b32fc
4GuptaArpit <2004arpitgupta@gmail.com>
2026-06-27 23:06:17 +0530

    07 : Final-check

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -19,9 +21,15 @@ app.use(cookieParser()); //allows to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port :", PORT);
 });
-
-// FrSoVq2urmE5538L
